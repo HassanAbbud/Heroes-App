033428f230418ec4038b266b48834eaf84ef1230
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-10 16:45:49 -0600

    Component creation & setup

---
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-signup-page',
+  templateUrl: './signup-page.component.html',
+
+})
+export class SignupPageComponent {
+
+}
---
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heroes-new-hero-page',
+  templateUrl: './new-hero-page.component.html',
+})
+export class NewHeroPageComponent {
+
+}
