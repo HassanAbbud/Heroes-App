5798be53ced8f7978a2256831644a56b672bbe42
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-11 13:32:45 -0600

    Display hero list from http request OnInit

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/heroe.interface';
+import { environments } from '../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  constructor() { }
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http:HttpClient) { }
+
+  getHeroes():Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
 }
