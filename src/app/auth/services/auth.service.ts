dddf57fa12b3c8ad2659a441027c07f36d27af95
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-12 17:48:52 -0600

    Mantain user session

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationService {
@@ -28,5 +28,18 @@ export class AuthenticationService {
   logOut(){
     this.user = undefined;
     localStorage.clear();
+  }
+
+  checkAuthenticationStatus(): Observable<boolean>{
+    if (localStorage.getItem('token')) return of(false);
+    const token = localStorage.getItem('token')
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false))
+      )
+    return of(true);
   }
 }
