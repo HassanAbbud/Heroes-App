cb1076490679b58e0ff933593adcd311ce44b59b
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-12 13:22:14 -0600

    Implemented edit hero pre-filled & reactiveform fix

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'heroes-new-hero-page',
   templateUrl: './new-hero-page.component.html',
 })
-export class NewHeroPageComponent {
+export class NewHeroPageComponent implements OnInit{
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('', {nonNullable: true}),
     publisher: new FormControl<Publisher>(Publisher.DCComics),
     alter_ego: new FormControl(''),
-    first_appearan: new FormControl(''),
+    first_appearance: new FormControl(''),
     characters: new FormControl(''),
     alt_img: new FormControl(''),
   });
@@ -24,11 +26,31 @@ export class NewHeroPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
-  constructor(private heroService:HeroesService){}
+  constructor(
+    private heroService:HeroesService,
+    private router:Router,
+    private activatedRoute: ActivatedRoute,
+  ){}
 
   get currentHero(): Hero{
     const hero = this.heroForm.value as Hero;
     return hero;
+  }
+
+  ngOnInit(): void {
+    if(!this.router.url.includes("edit")) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroService.getHeroById(id)),
+      ).subscribe(hero => {
+
+        if ( !hero ) {
+          return this.router.navigateByUrl('/');
+        }
+      this.heroForm.reset( hero );
+      return;
+    });
   }
 
   onSubmit(): void{
